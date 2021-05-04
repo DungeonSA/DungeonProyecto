f4fb9325980ead59209fe9c08809032adc616202
alverich <alrompin98@gmail.com>
2021-05-04 20:15:05 +0200

    Cambios en Clases Pantalla y Juego
    Rellenado Pantalla.java y Juego.java (sin completar

--- Dungeon/core/src/main/java/com/dungeonsa/Pantallas/Pantalla.java
+++ Dungeon/core/src/main/java/com/dungeonsa/Pantallas/Pantalla.java
@@ -1,4 +1,20 @@
 package com.dungeonsa.Pantallas;
 
-public class Pantalla {
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.dungeonsa.Juego;
+
+public abstract class Pantalla implements Screen {
+    protected Juego juego;
+    protected SpriteBatch sb;
+
+    public Pantalla() {
+        this.juego=Juego.getInstance();
+        this.sb= juego.getSb();
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
 }
