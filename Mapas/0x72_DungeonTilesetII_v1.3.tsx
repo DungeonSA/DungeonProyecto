e8decb4adb65d9bcb63794f5c74cc12f6e06b43f
BasilioLagares <basiliolagares@gmail.com>
2021-05-05 18:58:39 +0200

    Actualización Mapas 05/05/2021
    - Mejoras en Dungeon_Mapa1.
    - Creación de Dungeon_Final.
    - Inicio de creación de Demacia_Mapa (incompleto).

--- Mapas/0x72_DungeonTilesetII_v1.3.tsx
+++ Mapas/0x72_DungeonTilesetII_v1.3.tsx
@@ -1,4 +1,40 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.6.0" name="0x72_DungeonTilesetII_v1.3" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
  <image source="0x72_DungeonTilesetII_v1.3.1/0x72_DungeonTilesetII_v1.3.png" width="512" height="512"/>
+ <tile id="36">
+  <animation>
+   <frame tileid="36" duration="11"/>
+   <frame tileid="37" duration="11"/>
+   <frame tileid="38" duration="11"/>
+  </animation>
+ </tile>
+ <tile id="68">
+  <animation>
+   <frame tileid="68" duration="11"/>
+   <frame tileid="69" duration="11"/>
+   <frame tileid="70" duration="11"/>
+  </animation>
+ </tile>
+ <tile id="100">
+  <animation>
+   <frame tileid="100" duration="10"/>
+   <frame tileid="101" duration="10"/>
+   <frame tileid="102" duration="10"/>
+  </animation>
+ </tile>
+ <tile id="132">
+  <animation>
+   <frame tileid="132" duration="11"/>
+   <frame tileid="133" duration="11"/>
+   <frame tileid="134" duration="11"/>
+  </animation>
+ </tile>
+ <tile id="353">
+  <animation>
+   <frame tileid="353" duration="100"/>
+   <frame tileid="354" duration="100"/>
+   <frame tileid="355" duration="100"/>
+   <frame tileid="356" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
