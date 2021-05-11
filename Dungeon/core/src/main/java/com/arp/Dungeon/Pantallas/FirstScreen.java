50ed25703e13a08ed14a3fcb568eadd7067bd473
alverich <alrompin98@gmail.com>
2021-05-11 17:48:52 +0200

    Nueva Version con librerias de fisicas

--- Dungeon/core/src/main/java/com/arp/Dungeon/Pantallas/FirstScreen.java
+++ Dungeon/core/src/main/java/com/arp/Dungeon/Pantallas/FirstScreen.java
@@ -4,15 +4,30 @@ package com.dungeonsa.Pantallas;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.dungeonsa.Personajes.Jugador;
+import com.badlogic.gdx.physics.box2d.World;
+
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen extends Pantalla {
-	private Texture fondo;
+	public static final int LADO_LOSA=32;
+	private TiledMap mapa;
+	private OrthogonalTiledMapRenderer renderizador;
+	//Fisicas
+	private World mundo;
 
 
 	public FirstScreen() {
 		super();
+		mapa=am.get("");
+		renderizador=new OrthogonalTiledMapRenderer(mapa, 1.0f/LADO_LOSA);
+
+		//Fisicas
+		mundo=new World(new Vector2(0,-3),true);
+
+		//Procesa la capa 2 del mapa, la que tiene los "objetos"
 	}
 
 	@Override
@@ -30,14 +45,20 @@ public class FirstScreen extends Pantalla {
 
 	@Override
 	public void actualizar(float delta) {
-
+		renderizador.setView(camara);
+		camara.update();
 	}
 
 	@Override
 	public void dibujar(float delta) {
+		int[] capas={0,1};
+		renderizador.render(capas);
+		sb.setProjectionMatrix(camara.combined);
 		sb.begin();
 
 		sb.end();
+
+		depurador.render(mundo, camara.combined);
 	}
 
 	@Override
