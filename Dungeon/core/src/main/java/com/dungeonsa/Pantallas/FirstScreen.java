d640023406f8367e9a7457f598ef2f131c105de7
raulhidalgorodriguez <83598252+raulhidalgorodriguez@users.noreply.github.com>
2021-05-13 16:02:29 +0200

    i

--- Dungeon/core/src/main/java/com/dungeonsa/Pantallas/FirstScreen.java
+++ Dungeon/core/src/main/java/com/dungeonsa/Pantallas/FirstScreen.java
@@ -1,55 +1,18 @@
 package com.dungeonsa.Pantallas;
 
 
-import Entorno.Muro;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.maps.MapProperties;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.dungeonsa.Personajes.Jugador;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.graphics.Texture;
 
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen extends Pantalla {
-	public static final String TIPO="tipo";
-	public static final String JUGADOR="mainchar";
-	public static final String MURO="nada";
-	public static final int LADO_LOSA=16;
-	private TiledMap mapa;
-	private OrthogonalTiledMapRenderer renderizador;
-
-	public static final Vector2 PASO_ARRIBA=new Vector2(0,.1f);
-	public static final Vector2 PASO_ABAJO=new Vector2(0,-.1f);
-	public static final Vector2 PASO_DERECHA=new Vector2(.1f,0);
-	public static final Vector2 PASO_IZQUIERDA=new Vector2(-.1f,0);
-	public static float VEL_MAX=2.0f;
-	private Jugador jugador=null;
-	private Body cuerpoJugador=null;
-	private Vector2 posicionJugador=null;
-
-	TextureAtlas atlas;
-	//Fisicas
-	private World mundo;
-	private Box2DDebugRenderer depurador;
-
+	private Texture fondo;
 
 	public FirstScreen() {
-//		super();
-//		mapa=am.get("");
-//		renderizador=new OrthogonalTiledMapRenderer(mapa, 1.0f/LADO_LOSA);
-//
-//		//Fisicas
-//		mundo=new World(new Vector2(0,-3),true);
-//
-//		//Procesa la capa 2 del mapa, la que tiene los "objetos"
 		super();
+<<<<<<< HEAD
 		atlas=am.get("Graficos.atlas");
 		mapa=am.get("mapadev.tmx");
 		renderizador=new OrthogonalTiledMapRenderer(mapa,1.0f/LADO_LOSA);
@@ -79,46 +42,39 @@ public class FirstScreen extends Pantalla {
 				}
 			}
 		}
-	}
+=======
+		am.load("Menu.png", Texture.class);
+		am.finishLoading();
+		fondo=am.get("Menu.png");
 
-	@Override
-	public void leerEntrada(float delta) {
-		posicionJugador=cuerpoJugador.getPosition();
-//		if(Gdx.input.isKeyPressed(Input.Keys.A)){
-//			Jugador.moverIzquierda();
-//		}if(Gdx.input.isKeyPressed(Input.Keys.W)){
-//			Jugador.moverArriba();
-//		}if(Gdx.input.isKeyPressed(Input.Keys.S)){
-//			Jugador.moverAbajo();
-//		}if(Gdx.input.isKeyPressed(Input.Keys.D)){
-//			Jugador.moverDerecha();
-//		}
-	}
-
-	@Override
-	public void actualizar(float delta) {
-		renderizador.setView(camara);
-		camara.update();
-	}
-
-	@Override
-	public void dibujar(float delta) {
-		int[] capas={0,1};
-		renderizador.render(capas);
-
-		sb.setProjectionMatrix(camara.combined);
-		sb.begin();
-
-		sb.end();
-
-		depurador.render(mundo, camara.combined);
-		mundo.step(.02f,6,2);
 	}
 
 	@Override
 	public void show() {
 
+>>>>>>> 67b8a983cd7e3f53b8a670575a8eff9aab2edb73
 	}
+
+	@Override
+	public void leerEntrada(float delta) {
+		if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)){
+			juego.cambiarPantalla(this,new Pantalladev());
+		}else if(Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)){
+			juego.cambiarPantalla(this,new PantallaRome());
+		}
+	}
+
+	@Override
+	public void actualizar(float delta) {
+	}
+
+	@Override
+	public void dibujar(float delta) {
+		sb.begin();
+		sb.draw(fondo,0,0,juego.getAncho(),juego.getAlto());
+		sb.end();
+	}
+
 
 	@Override
 	public void resize(int width, int height) {
